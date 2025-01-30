2c4ec317892f7d2bc07a299d83d8ec766729eea7
lumi <nwosupaulchinagorom@gmail.com>
2025-01-30 14:15:37 +0100

    Book app api

--- controllers/user.js
+++ controllers/user.js
@@ -1,34 +1,33 @@
-const UserModel = require('../models/user');
+const UserModel = require("../model/user");
 
-async function createUser (req, res) {
-    // create a user name
+async function createUser(req, res) {
+  // create a user name
 
-    req.body.username = req.body.email.split('@')[0];
-    // set isAdult using the age
-    req.body.isAdult = req.body.age < 20 ? false : true;
-    // create the user
-    await UserModel.create(req.body);
+  req.body.username = req.body.email.split("@")[0];
+  // set isAdult using the age
+  req.body.isAdult = req.body.age < 20 ? false : true;
+  // create the user
+  await UserModel.create(req.body);
 
-    res.json({
-        message: "success!!!"
-    })
-};
-
-async function getAllUsers (req, res) {
-    const users = await UserModel.find({});
-    res.json({
-        message: "success!!!",
-        data: users
-    })
+  res.json({
+    message: "success!!!",
+  });
 }
 
+async function getAllUsers(req, res) {
+  const users = await UserModel.find({});
+  res.json({
+    message: "success!!!",
+    data: users,
+  });
+}
 
-async function getSingleUser (req, res) {
-    const user = await UserModel.findById(req.params.id);
-    res.json({
-        message: "success!!!",
-        data: user
-    })
+async function getSingleUser(req, res) {
+  const user = await UserModel.findById(req.params.id);
+  res.json({
+    message: "success!!!",
+    data: user,
+  });
 }
 
 module.exports = { createUser, getAllUsers, getSingleUser };
--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,12 @@
-const express = require("express")
-const userController = require('../controllers/user')
+const express = require("express");
+const userController = require("../controllers/user");
 
-const router = express.Router()
+const router = express.Router();
 
+router.post("/user", userController.createUser);
 
+router.get("/users", userController.getAllUsers);
 
-router.post('/user', userController.createUser);
-
-
-router.get('/users', userController.getAllUsers)
-
-
-router.get('/:id', userController.getSingleUser)
+router.get("/:id", userController.getSingleUser);
 
 module.exports = router;
-  
